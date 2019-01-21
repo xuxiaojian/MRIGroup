1cbe8e2581335f76160c52a858347de4d7336956
weijie.gan <weijie.gan@wustl.edu>
2019-01-21 17:40:21 -0600

    Jan21 重构代码前的备份

--- weijie/main.py
+++ weijie/main.py
@@ -1,47 +1,87 @@
 import configparser
 import os
-import sys
-from net import unet, sr
 from util import dataLoader
 import numpy as np
 
-# Set Config File
 config = configparser.ConfigParser()
 config.read('./config.ini')
 
 # Set Global Variables
 os.environ['CUDA_VISIBLE_DEVICES'] = config['GLOBAL']['index_gpu']
-print('Using [', np.fromstring(config['GLOBAL']['index_gpu'], dtype=int, sep=',').shape[0],
-      '] GPUs. The Index: [ ', config['GLOBAL']['index_gpu'], ' ]')
+print('GPU Index: [ ' + config['GLOBAL']['index_gpu'] + ' ]')
 
 dataset_path = config['GLOBAL']['dataset_path']
+dataset_type = config['GLOBAL']['dataset_type']
 
-if len(sys.argv) == 3 and str(sys.argv[1]) == 'unet':
+network = config['GLOBAL']['network']
+method = config['GLOBAL']['method']
 
-    if str(sys.argv[2]) == 'train':
+if method == 'train':
 
-        net = unet.TFNetwork(config)
-        x_train, y_train, x_val, y_val = dataLoader.train_unet(dataset_path)
-        net.train(x_train, y_train, x_val, y_val, display=True)
+    index_train_mat = np.fromstring(config['GLOBAL']['index_train_mat'], dtype=np.int, sep=',').tolist()
+    index_train_imgs = np.fromstring(config['GLOBAL']['index_train_imgs'], dtype=np.int, sep=',').tolist()
 
-if len(sys.argv) == 3 and str(sys.argv[1]) == 'sr':
+    index_valid_mat = np.fromstring(config['GLOBAL']['index_valid_mat'], dtype=np.int, sep=',').tolist()
+    index_valid_imgs = np.fromstring(config['GLOBAL']['index_valid_imgs'], dtype=np.int, sep=',').tolist()
 
-    if str(sys.argv[2]) == 'train':
-        print(0)
-        # x_train_noised, y_train, x_val_noised, y_val = dataLoader.train_sr(dataset_path)
-        #
-        # # Extract Feature Map from UNet
-        # unet_model_path = '/home/xiaojianxu/gan/experiment/dec31/unet_ver3/final/model.cpkt'
-        # net = unet.TFNetwork()
-        # print('Extract Feature of Training data')
-        # x_train_feature = net.predict(unet_model_path, x_train_noised, 1, True, batchsize=48)
-        # print('Extract Feature of Validation data')
-        # x_val_feature = net.predict(unet_model_path, x_val_noised, 1, True, batchsize=1)
-        #
-        # # Concatenate all Feature
-        # x_train = np.concatenate([x_train_noised, x_train_feature], -1)
-        # x_val = np.concatenate([x_val_noised, x_val_feature], -1)
-        #
-        # # Begin Training
-        # net = sr.KerasNetwork(config, channel=2, num_gpu=num_gpu)
-        # net.train(x_train, y_train, x_val, y_val)
+    x_train, y_train, x_val, y_val, x_train_imgs, y_train_imgs, x_val_imgs, y_val_imgs = dataLoader.trainNvalid(
+        dataset_path, dataset_type, index_train_mat, index_train_imgs, index_valid_mat, index_valid_imgs)
+
+    if network == 'unet':
+
+        from scadec import image_util, unet_bn, train
+
+        data_provider = image_util.SimpleDataProvider(x_train, y_train)
+        valid_provider = image_util.SimpleDataProvider(x_val, y_val)
+
+        data_channels = 1
+        truth_channels = 1
+
+        ####################################################
+        #                     NETWORK                      #
+        ####################################################
+
+        """
+            here we specify the neural network.
+        """
+
+        # -- Network Setup -- #
+        # set up args for the unet
+        kwargs = {
+            "layers": 5,           # how many resolution levels we want to have
+            "conv_times": 2,       # how many times we want to convolve in each level
+            "features_root": 64,
+            # how many feature_maps we want to have as root (the following levels will calculate the
+            # feature_map by multiply by 2, exp, 64, 128, 256)
+            "filter_size": 3,      # filter size used in convolution
+            "pool_size": 2,        # pooling size used in max-pooling
+            "summaries": True
+        }
+
+        net = unet_bn.Unet_bn(img_channels=data_channels, truth_channels=truth_channels,
+                              cost="mean_squared_error", **kwargs)
+
+        batch_size = int(config['UNET']['batch_size'])  # batch size for training
+        valid_size = int(config['UNET']['valid_size'])  # batch size for validating
+        optimizer = "adam"  # optimizer we want to use, 'adam' or 'momentum'
+
+        # output paths for results
+        output_path = config['UNET']['save_path'] + '/models'
+        prediction_path = config['UNET']['save_path'] + '/validation'
+        # restore_path = 'gpu001/models/50099_cpkt'
+
+        # optional args
+        opt_kwargs = {
+            'learning_rate': float(config['UNET']['learning_rate'])
+        }
+
+        # make a trainer for scadec
+        trainer = train.Trainer_bn(net, batch_size=batch_size, optimizer="adam", opt_kwargs=opt_kwargs)
+        path = trainer.train(data_provider, output_path, valid_provider, valid_size,
+                             x_train_imgs, y_train_imgs, x_val_imgs, y_val_imgs,
+                             training_iters=int(config['UNET']['training_iters']),
+                             epochs=int(config['UNET']['epochs']),
+                             save_epoch=int(config['UNET']['save_epoch']),
+                             dropout=float(config['UNET']['dropout']),
+                             prediction_path=prediction_path,
+                             display_step=1)
--- weijie/util/dataLoader.py
+++ weijie/util/dataLoader.py
@@ -2,81 +2,41 @@ import numpy as np
 import scipy.io as sio
 
 
-def train_sr(root_path):
-    index = [5, 6, 7, 8]
-    num_imgs = 960
+def trainNvalid(root_path, type_, index_train_mat, index_train_imgs, index_valid_mat, index_valid_imgs):
 
-    print('Begin Loading Train Data....')
-    height = 320
-    width = 320
-    size_batch = num_imgs * index.__len__()
-    x_train = np.zeros([size_batch, height, width])
-    y_train = np.zeros([size_batch, height, width])
+    if not(type_ == 'mri' or type_ == 'liver'):
+        print('[ERROR]: WRONG TYPE')
+        exit(0)
 
-    count = 0
-    index_count = 1
-    for i in index:
-        print('[%d] th, [%d] Total' % (index_count, index.__len__()))
-        index_count = index_count + 1
-        print('The File Path:' + root_path + 'mat/' + str(i) + '.mat')
-        mats = sio.loadmat(root_path + 'mat/' + str(i) + '.mat')
-        nims = mats['nims']
-        cims = mats['cims']
-
-        for j in range(num_imgs):
-            x_train[count, :, :] = nims[:, :, j]
-            y_train[count, :, :] = cims[:, :, j]
-            count = count + 1
-
-    x_train.shape = [size_batch, height, width, 1]
-    y_train.shape = [size_batch, height, width, 1]
-
-    # Obtain Validation Data
-    print('Begin Loading Validation Data....')
-    print('The File Path:' + root_path + 'mat/9.mat')
-    mats = sio.loadmat(root_path + 'mat/9.mat')
-    nims = mats['nims']
-    cims = mats['cims']
-
-    index = [10, 15, 20, 25, 30, 35]
-    height, width, _ = nims.shape
-    size_batch = index.__len__()
-
-    x_val = np.zeros([size_batch, height, width])
-    y_val = np.zeros([size_batch, height, width])
-
-    for i in range(size_batch):
-        x_val[i, :, :] = nims[:, :, index[i]]
-        y_val[i, :, :] = cims[:, :, index[i]]
-
-    x_val.shape = [size_batch, height, width, 1]
-    y_val.shape = [size_batch, height, width, 1]
-
-    return x_train, y_train, x_val, y_val
-
-
-def train_unet(root_path):
-    index = [1]
-
+    ####################################################
+    #                obtain Training Data              #
+    ####################################################
     print('Begin Loading Train Data....')
     x_train = []
     y_train = []
 
-    # count = 0
     index_count = 1
-    for i in index:
-        print('[%d] th, [%d] Total' % (index_count, index.__len__()))
+    for i in index_train_mat:
+        print('[%d] th, [%d] Total' % (index_count, index_train_mat.__len__()))
         index_count = index_count + 1
-        print('The File Path:' + root_path + 'mat/' + str(i) + '.mat')
-        mats = sio.loadmat(root_path + 'mat/' + str(i) + '.mat')
+
+        print('The File Path:' + root_path + type_ + '/mat/' + str(i) + '.mat')
+        mats = sio.loadmat(root_path + type_ + '/mat/' + str(i) + '.mat')
         nims = mats['nims']
         cims = mats['cims']
 
-        num_imgs = nims.shape[0]
+        if type_ == 'liver':
+            num_imgs = nims.shape[0]
+        else:
+            num_imgs = nims.shape[2]
+
         for j in range(num_imgs):
-            x_train.append(nims[j, :, :])
-            y_train.append(cims[j, :, :])
-            # count = count + 1
+            if type_ == 'liver':
+                x_train.append(nims[j, :, :])
+                y_train.append(cims[j, :, :])
+            else:
+                x_train.append(nims[:, :, j])
+                y_train.append(cims[:, :, j])
 
     x_train = np.array(x_train)
     y_train = np.array(y_train)
@@ -84,109 +44,65 @@ def train_unet(root_path):
     x_train.shape = [x_train.shape[0], x_train.shape[1], x_train.shape[2], 1]
     y_train.shape = [y_train.shape[0], y_train.shape[1], y_train.shape[2], 1]
 
+    ####################################################
+    #                obtain Validation Data            #
+    ####################################################
+
     # Obtain Validation Data
     print('Begin Loading Validation Data....')
-    print('The File Path:' + root_path + 'mat/9.mat')
-    mats = sio.loadmat(root_path + 'mat/9.mat')
-    nims = mats['nims']
-    cims = mats['cims']
 
-    index = [10, 15, 20, 25, 30, 35]
-    _, height, width = nims.shape
-    size_batch = index.__len__()
+    x_val = []
+    y_val = []
 
-    x_val = np.zeros([size_batch, height, width])
-    y_val = np.zeros([size_batch, height, width])
+    index_count = 1
+    for i in index_valid_mat:
+        print('[%d] th, [%d] Total' % (index_count, index_valid_mat.__len__()))
+        index_count = index_count + 1
 
-    for i in range(size_batch):
-        x_val[i, :, :] = nims[index[i], :, :]
-        y_val[i, :, :] = cims[index[i], :, :]
+        print('The File Path:' + root_path + type_ + '/mat/' + str(i) + '.mat')
+        mats = sio.loadmat(root_path + type_ + '/mat/' + str(i) + '.mat')
+        nims = mats['nims']
+        cims = mats['cims']
 
-    x_val.shape = [size_batch, height, width, 1]
-    y_val.shape = [size_batch, height, width, 1]
-
-    return x_train, y_train, x_val, y_val
-
-
-# noinspection PyTupleAssignmentBalance
-class BaseDataProvider(object):
-
-    def __init__(self, a_min=None, a_max=None):
-        self.a_min = a_min if a_min is not None else -np.inf
-        self.a_max = a_max if a_min is not None else np.inf
-
-    def __call__(self, n, fix=False):
-        if type(n) == int and not fix:
-            # X and Y are the images and truths
-            train_data, truths = self._next_batch(n)
-        elif type(n) == int and fix:
-            train_data, truths = self._fix_batch(n)
-        elif type(n) == str and n == 'full':
-            train_data, truths = self._full_batch()
+        if type_ == 'liver':
+            num_imgs = nims.shape[0]
         else:
-            raise ValueError("Invalid batch_size: " % n)
+            num_imgs = nims.shape[2]
 
-        return train_data, truths
+        for j in range(num_imgs):
+            if type_ == 'liver':
+                x_val.append(nims[j, :, :])
+                y_val.append(cims[j, :, :])
+            else:
+                x_val.append(nims[:, :, j])
+                y_val.append(cims[:, :, j])
 
-    def _next_batch(self, n):
-        pass
+    x_val = np.array(x_val)
+    y_val = np.array(y_val)
 
-    def _full_batch(self):
-        pass
+    x_val.shape = [x_val.shape[0], x_val.shape[1], x_val.shape[2], 1]
+    y_val.shape = [y_val.shape[0], y_val.shape[1], y_val.shape[2], 1]
 
-    def _fix_batch(self, n):
-        pass
+    ####################################################
+    #                     obtain  Imgs                 #
+    ####################################################
 
+    x_train_imgs = []
+    y_train_imgs = []
+    x_val_imgs = []
+    y_val_imgs = []
 
-class SimpleDataProvider(BaseDataProvider):
+    for i in index_train_imgs:
+        x_train_imgs.append(x_train[i, :, :, :])
+        y_train_imgs.append(y_train[i, :, :, :])
 
-    def __init__(self, data, truths):
-        super(SimpleDataProvider, self).__init__()
-        self.data = np.float64(data)
-        self.truths = np.float64(truths)
-        self.img_channels = self.data[0].shape[2]
-        self.truth_channels = self.truths[0].shape[2]
-        self.file_count = data.shape[0]
+    for i in index_valid_imgs:
+        x_val_imgs.append(x_val[i, :, :, :])
+        y_val_imgs.append(y_val[i, :, :, :])
 
-    def _next_batch(self, n):
-        idx = np.random.choice(self.file_count, n, replace=False)
-        img = self.data[idx[0]]
-        nx = img.shape[0]
-        ny = img.shape[1]
-        X = np.zeros((n, nx, ny, self.img_channels))
-        Y = np.zeros((n, nx, ny, self.truth_channels))
-        for i in range(n):
-            X[i] = self._process_data(self.data[idx[i]])
-            Y[i] = self._process_truths(self.truths[idx[i]])
-        return X, Y
+    x_train_imgs = np.array(x_train_imgs)
+    y_train_imgs = np.array(y_train_imgs)
+    x_val_imgs = np.array(x_val_imgs)
+    y_val_imgs = np.array(y_val_imgs)
 
-    def _fix_batch(self, n):
-        # first n data
-        img = self.data[0]
-        nx = img.shape[0]
-        ny = img.shape[1]
-        X = np.zeros((n, nx, ny, self.img_channels))
-        Y = np.zeros((n, nx, ny, self.truth_channels))
-        for i in range(n):
-            X[i] = self._process_data(self.data[i])
-            Y[i] = self._process_truths(self.truths[i])
-        return X, Y
-
-    def _full_batch(self):
-        return self.data, self.truths
-
-    def _process_truths(self, truth):
-        # normalization by channels
-        truth = np.clip(np.fabs(truth), self.a_min, self.a_max)
-        for channel in range(self.truth_channels):
-            truth[:, :, channel] -= np.amin(truth[:, :, channel])
-            truth[:, :, channel] /= np.amax(truth[:, :, channel])
-        return truth
-
-    def _process_data(self, data):
-        # normalization by channels
-        data = np.clip(np.fabs(data), self.a_min, self.a_max)
-        for channel in range(self.img_channels):
-            data[:, :, channel] -= np.amin(data[:, :, channel])
-            data[:, :, channel] /= np.amax(data[:, :, channel])
-        return data
+    return x_train, y_train, x_val, y_val, x_train_imgs, y_train_imgs, x_val_imgs, y_val_imgs
