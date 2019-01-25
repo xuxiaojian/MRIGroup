e1a24aea1e75a88926150dbf5ebb786821f3a588
weijie.gan <weijie.gan@wustl.edu>
2019-01-24 21:38:35 -0600

    Fit it for Base Network Class

--- weijie/config.py
+++ weijie/config.py
@@ -1,39 +1,54 @@
 import tensorflow as tf
-import os
+
+# ###############################
+#       USER DEFINE PAREMETERS
+# ###############################
+user_path = '/export/project/gan.weijie/'
 
 # GLOBAL
-tf.flags.DEFINE_string('gpu_index', '2', 'Choice what GPUs tf can use')
+tf.flags.DEFINE_string('gpu_index', '3', 'Choice what GPUs tf can use')
 tf.flags.DEFINE_string('network', 'unet', '')
 tf.flags.DEFINE_string('mode', 'train', '')
 
-# DATASET
-tf.flags.DEFINE_string('root_path', '/home/xiaojianxu/gan/data/', '')
-tf.flags.DEFINE_string('dataset_type', 'liver', '')
-
-tf.flags.DEFINE_list('index_train_unet_mat', [1], '')
-tf.flags.DEFINE_list('index_train_unet_images', [15], '')
-
-tf.flags.DEFINE_list('index_train_sr_mat', [5, 6], '')
-tf.flags.DEFINE_list('index_train_sr_images', [15], '')
+tf.flags.DEFINE_string('root_path', user_path + 'data/', '')
+tf.flags.DEFINE_string('dataset_type', 'mri_healthy_liver', '')
 
 tf.flags.DEFINE_list('index_valid_mat', [9], '')
-tf.flags.DEFINE_list('index_valid_images', [0, 1, 2, 3], '')
+tf.flags.DEFINE_list('index_valid_images', [15, 30, 35], '')
 
 # UNET
-tf.flags.DEFINE_string('unet_output_path', '/home/xiaojianxu/gan/experiment/jan22/unet_ver2/', '')
-tf.flags.DEFINE_integer('unet_batch_size', 8, '')
-tf.flags.DEFINE_integer('unet_epoch', 300, '')
-tf.flags.DEFINE_integer('unet_epoch_save_model', 50, '')
-tf.flags.DEFINE_integer('unet_epoch_save_val', 10, '')
-tf.flags.DEFINE_float('unet_learning_rate', 0.001, '')
+config_name = 'unet_'
 
-tf.flags.DEFINE_list('unet_net_input_shape', [None, None, 1], '')
-tf.flags.DEFINE_integer('unet_net_level', 3, '')
-tf.flags.DEFINE_integer('unet_net_root_filters', 32, '')
-tf.flags.DEFINE_float('unet_net_dropout', 0.1, '')
-tf.flags.DEFINE_integer('unet_net_kernel_size', 3, '')
+tf.flags.DEFINE_list(config_name + 'index_train_mat', [1], '')
+tf.flags.DEFINE_list(config_name + 'index_train_images', [15], '')
 
-# ############################
-#       Set up GPUs
-# ############################
+tf.flags.DEFINE_string(config_name + 'output_path',
+                       user_path + '/experiment/jan24/temp/', '')
+tf.flags.DEFINE_integer(config_name + 'batch_size', 2, '')
+tf.flags.DEFINE_integer(config_name + 'epoch', 300, '')
+tf.flags.DEFINE_integer(config_name + 'epoch_save_model', 50, '')
+tf.flags.DEFINE_integer(config_name + 'epoch_save_val', 10, '')
+tf.flags.DEFINE_float(config_name + 'learning_rate', 0.001, '')
+
+tf.flags.DEFINE_list(config_name + 'input_shape', [None, None, 1], '')
+tf.flags.DEFINE_integer(config_name + 'level', 5, '')
+tf.flags.DEFINE_integer(config_name + 'root_filters', 64, '')
+tf.flags.DEFINE_float(config_name + 'dropout', 0.1, '')
+tf.flags.DEFINE_integer(config_name + 'kernel_size', 3, '')
+tf.flags.DEFINE_integer(config_name + 'conv_time', 2, '')
+
+# SR
+config_name = 'sr_'
+
+tf.flags.DEFINE_list(config_name + 'index_train_mat', [4, 5, 6, 7], '')
+tf.flags.DEFINE_list(config_name + 'index_train_images', [15], '')
+
+
+# ###############################
+#       Set up GPUs Environment
+# ###############################
+
+import os
 os.environ['CUDA_VISIBLE_DEVICES'] = tf.flags.FLAGS.gpu_index
+import numpy as np
+tf.flags.DEFINE_integer('gpu_num', np.fromstring(tf.flags.FLAGS.gpu_index, dtype=np.int, sep=',').shape[0], '')
