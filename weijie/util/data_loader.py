c44cb4642f4c3102177f0a960d35acb6c20ec317
weijie.gan <weijie.gan@wustl.edu>
2019-01-24 21:36:57 -0600

    Add Crop Method Implemented by Numpy

--- weijie/util/data_loader.py
+++ weijie/util/data_loader.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.io as sio
 from util.img_process import normalize
+from skimage.util import view_as_windows
 
 
 # noinspection PyUnresolvedReferences
@@ -11,31 +12,28 @@ def mat2numpy(root_path, type_, index_mat, index_images):
     # If necessary, you can change it to show your     #
     # custom method of data loading.                   #
     ####################################################
-    if not (type_ == 'mri' or type_ == 'liver'):
-        print('[ERROR]: WRONG TYPE')
-        exit(0)
-
     data = []
     ground_truth = []
 
     for i in range(index_mat.__len__()):
-        path_file = root_path + type_ + '/mat/' + str(index_mat[i]) + '.mat'
+        path_file = root_path + type_ + '/' + str(index_mat[i]) + '.mat'
         print('[%d] th, [%d] Total. ' % (i+1, index_mat.__len__()) + 'The Path of File:' + path_file)
 
         mats = sio.loadmat(path_file)
         nims = mats['nims']
         cims = mats['cims']
 
-        if type_ == 'liver':
+        num_images = None
+        if type_ == 'mri_healthy_liver':
             num_images = nims.shape[0]
-        else:
+        if type_ == 'mri_healthy_all':
             num_images = nims.shape[2]
 
         for j in range(num_images):
-            if type_ == 'liver':
+            if type_ == 'mri_healthy_liver':
                 data.append(normalize(nims[j, :, :]))
                 ground_truth.append(normalize(cims[j, :, :]))
-            else:
+            if type_ == 'mri_healthy_all':
                 data.append(normalize(nims[:, :, j]))
                 ground_truth.append(normalize(cims[:, :, j]))
 
@@ -57,3 +55,18 @@ def mat2numpy(root_path, type_, index_mat, index_images):
         ground_truth_images[i] = ground_truth[index_images[i]]
 
     return data, ground_truth, data_images, ground_truth_images
+
+
+def crop_images(imgs, window_size, step):
+    batch_size = imgs.shape[0]
+    channel = imgs.shape[3]
+
+    crop_imgs = view_as_windows(imgs, window_shape=[batch_size, window_size, window_size, channel], step=step)
+
+    crop_num_width = crop_imgs.shape[1]
+    crop_num_height = crop_imgs.shape[2]
+
+    crop_imgs = np.ascontiguousarray(crop_imgs, dtype=np.float64)
+    crop_imgs.shape = [crop_num_width * crop_num_height * batch_size, window_size, window_size, channel]
+
+    return crop_imgs
